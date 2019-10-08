2e429098e2f232346b4b118d885c90c1ff2cd7f2
lesliec1071 <56267101+lesliec1071@users.noreply.github.com>
2019-10-08 11:52:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Leslie"
+let userName = "lesliec1071";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
